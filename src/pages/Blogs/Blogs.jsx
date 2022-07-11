766a36017a490a4e81bb333851d3cb246c27725f
Monalissa Chiwara <monachiwara04@gmail.com>
2022-07-11 08:24:31 -0500

    auth functioning

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react'
 import { getBlogs } from '../../utilities/blogs-service'
-
+import { Link, useNavigate } from 'react-router-dom'
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([])
+
+    const navigate = useNavigate()
+
     useEffect(() => {
         // console.log('hey')
         (async () => {
@@ -13,8 +16,28 @@ const Blogs = () => {
     }, [])
   
     return (
-        <div>
-            this is our blog page
+        <div className="h-100" id='blog-wrapper'>
+            <Link className="btn btn-primary" to='/blogs/create'>Add Blog</Link>
+
+            <div id='blog-container'>
+                {
+                    blogs.map(blog =>
+                        <div 
+                            className="card" 
+                            id='blog-card'
+                            key={blog._id}
+                            onClick={() => navigate(`/blogs/${blog._id}`, { state: blog })}
+                        >
+                            <img src={blog.image} className="card-img-top" alt={`Blog poster: ${blog.title}`} id='blog-poster' />
+                            <div className="card-body">
+                                <h5 className="card-title">{blog.title}</h5>
+                                <p className="card-text">{blog.author}</p>
+                            </div>
+                        </div>
+                    )
+                }
+
+            </div>
         </div>
     );
 }
